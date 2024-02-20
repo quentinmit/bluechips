b0c022d78e77c75998db156841ead62c6d95b1ef
Quentin Smith <quentin@mit.edu>
2024-02-20 17:02:36 -0500

    Implement migration to create tables

---
+++ bluechips-rs/migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,49 @@
+use sea_orm_migration::prelude::*;
+use sea_orm::Schema;
+use sea_orm_migration::sea_orm::EntityName;
+use bluechips_rs::entities::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        let schema = Schema::new(manager.get_database_backend());
+        manager
+            .create_table(schema.create_table_from_entity(user::Entity))
+            .await?;
+        manager
+            .create_table(schema.create_table_from_entity(split::Entity))
+            .await?;
+        manager
+            .create_table(schema.create_table_from_entity(subitem::Entity))
+            .await?;
+        manager
+            .create_table(schema.create_table_from_entity(expenditure::Entity))
+            .await?;
+        manager
+            .create_table(schema.create_table_from_entity(transfer::Entity))
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .drop_table(Table::drop().table(transfer::Entity).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(expenditure::Entity).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(subitem::Entity).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(split::Entity).to_owned())
+            .await?;
+        manager
+            .drop_table(Table::drop().table(user::Entity).to_owned())
+            .await
+    }
+}
